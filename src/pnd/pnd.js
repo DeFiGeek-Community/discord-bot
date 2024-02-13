edcac6b8c1a9d0d32e63ea958310bf35840047b4
take3315 <114322307+take3315@users.noreply.github.com>
2024-02-13 14:32:22 +0900

    add poolConfig.js and update pnd.js (removed each token.js)

--- src/pnd/pnd.js
+++ src/pnd/pnd.js
@@ -1,14 +1,48 @@
-const pndCjpyRun = require("./cjpy.js");
-const pndEthRun = require("./eth.js");
-const pndCrvusdRun = require("./crvusd.js");
-const pndUsdcRun = require("./usdc.js");
+const Discord = require("discord.js");
+const { ethers, provider, intervalTime } = require("../util.js");
+const poolConfigs = require("../pnd/poolConfig.js");
 
 const pndUtilRun = () => {
   console.log(`executing pndUtilRun`);
-  pndCjpyRun();
-  pndEthRun();
-  pndCrvusdRun();
-  pndUsdcRun();
+  poolConfigs.forEach((config) => {
+    const pndUtilClient = new Discord.Client();
+    pndUtilClient.on("ready", async () => {
+      setInterval(async () => {
+        try {
+          const guild = pndUtilClient.guilds.cache.get(
+            process.env.DISCORD_CHANNEL_ID
+          );
+          const bot = await guild.members.fetch(config.botId);
+          const ABI = require("../../abi/pnd.json");
+          const contract = new ethers.Contract(
+            config.contractAddress,
+            ABI,
+            provider
+          );
+
+          const utilUint = await contract.getUtilization();
+          const util = Math.round(utilUint / 1e16);
+          const borrowRateUint = await contract.getBorrowRate(utilUint);
+          const supplyRateUint = await contract.getSupplyRate(utilUint);
+          const borrowRate = (
+            Math.round((borrowRateUint * 31536000) / 1e15) / 10
+          ).toFixed(1);
+          const supplyRate = (
+            Math.round((supplyRateUint * 31536000) / 1e15) / 10
+          ).toFixed(1);
+
+          await bot.setNickname(`${config.nicknamePrefix}: ${util}%`);
+          await pndUtilClient.user.setActivity(
+            `金利 貸:${supplyRate}% 借:${borrowRate}%`
+          );
+        } catch (err) {
+          console.log(err.name + ": " + err.message);
+        }
+      }, intervalTime);
+    });
+
+    pndUtilClient.login(config.botToken);
+  });
 };
 
 module.exports = pndUtilRun;
---
+++ src/pnd/poolConfig.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    contractAddress: "0xaa59f501c92092e624d30cff77eaff5ea4e7bfa2",
+    botId: process.env.DISCORD_PND_CJPY_BOT_ID,
+    botToken: process.env.DISCORD_PND_CJPY_BOT_TOKEN,
+    nicknamePrefix: "CJPY-util",
+  },
+  {
+    contractAddress: "0xE0e2d860797eF02F18c474847787A6C8f8389044",
+    botId: process.env.DISCORD_PND_CRVUSD_BOT_ID,
+    botToken: process.env.DISCORD_PND_CRVUSD_BOT_TOKEN,
+    nicknamePrefix: "CRVUSD-util",
+  },
+  {
+    contractAddress: "0x2bf8B07e41572FB09d9F002b2500E3eeDC5d7922",
+    botId: process.env.DISCORD_PND_ETH_BOT_ID,
+    botToken: process.env.DISCORD_PND_ETH_BOT_TOKEN,
+    nicknamePrefix: "ETH-util",
+  },
+  {
+    contractAddress: "0x7a997e2D7C84abb219832d944141de210e2840e1",
+    botId: process.env.DISCORD_PND_USDC_BOT_ID,
+    botToken: process.env.DISCORD_PND_USDC_BOT_TOKEN,
+    nicknamePrefix: "USDC-util",
+  },
+];
