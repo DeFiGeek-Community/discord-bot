c6edcbeba329337b9b4a513e4248d0fc0445d72e
0xHaku <32897506+0xHaku@users.noreply.github.com>
2024-07-07 12:50:45 +0900

    add health-check path

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/txjp", txjp);
 
+app.get("/health", (req, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
